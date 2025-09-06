3723c98857f8edf197a178b9ada76edb5af0d5c4
khaled ghaly <www.gmiza2967804@gmail.com>
2025-09-06 17:44:14 +0300

    Add Video

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-    document.getElementById('current-year').textContent = new Date().getFullYear();
+document.getElementById('current-year').textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("heroVideo");
+  const videoSource = document.getElementById("heroVideoSource");
+  const loadingCircle = document.getElementById("loadingCircle");
+
+  if (video && videoSource && loadingCircle) {
+    // 1. Set the source and poster
+    // video.poster = "img/terra.png";
+    videoSource.src = "https://github.com/khaled-hassan1/terra_feed/releases/download/intro/terra.mp4";
+    video.load();
+
+    // 2. Add event listener to hide loading circle and show video when ready
+    video.addEventListener('canplaythrough', () => {
+      loadingCircle.classList.add('hidden');
+      video.classList.add('video-visible');
+      video.play().catch(error => {
+        console.log("Autoplay issue:", error);
+      });
+    }, { once: true });
+
+    // 3. Autoplay logic for visibility changes
+    document.addEventListener("visibilitychange", function () {
+      if (document.visibilityState === "visible") {
+        video.play().catch(error => console.log("Autoplay issue:", error));
+      }
+    });
+
+    // 4. Autoplay logic for unexpected pauses
+    video.addEventListener("pause", function () {
+      if (document.visibilityState === "visible") {
+        video.play().catch(error => console.log("Autoplay issue:", error));
+      }
+    });
+  } else {
+    console.error("Hero video, video source, or loading circle element not found!");
+  }
+});
